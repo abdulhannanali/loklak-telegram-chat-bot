dca4493e357e5690410b589a5affbc57f75697f5
Abdul Hannan Ali <abdulhannanali@outlook.com>
2015-12-24 14:27:25 +0500

    setup redis in index.js for dev and productions also add some emojis

--- index.js
+++ index.js
@@ -6,15 +6,24 @@
  */
 const TelegramBot = require('node-telegram-bot-api')
 const lodash = require('lodash')
+const bluebird = require('bluebird')
+const redis = require('redis')
 
 var loklakBot;
-
+var redisClient;
 // Configurations specific to the development environment
 if (process.env.NODE_ENV == 'development') {
  // in the case of development environment
  // place the keys in config/keys.js file
  // see README.md for more help
  require('./config/keys.js')()
+
+ // local redis database incase of development
+ var redisClient = redis.createClient()
+ redisClient.on('error', function (error) {
+   console.error(error)
+   process.exit(1)
+ })
 
  // in case of development mode polling
  try {
@@ -36,6 +45,8 @@ else {
  const PORT = process.env.PORT || 443
  const HOST = process.env.HOST || '0.0.0.0'
  const externalUrl = process.env.EXTERNAL_URL || 'https://rocky-coast-3915.herokuapp.com'
+ const redisUrl = process.env.REDIS_DB_URL || ''
+ const redisPass = process.env.REDIS_DB_PASS || ''
 
  try {
    // setting up webhook in case of production
@@ -53,7 +64,27 @@ else {
    console.log('Error occured while setting up telegram webhook in production')
    console.error(error);
  }
+
+ // connecting to the redis database on the url
+ redisClient = redis.createClient(redisUrl, {})
+ if (redisPass) {
+   redis.auth(redisPass, (error) => {
+     console.error(error)
+     process.exit(1)
+   })
+ }
+
+ redisClient.on('error', (error) => {
+   console.log('Failed to connect to the redis database')
+   console.log("URL = " + redisUrl)
+   console.log("Pass ="  + redisPass)
+   console.error(error)
+   process.exit(1)
+ })
 }
 
-// loklakBotModules
-require('./lib/loklakTelegram.js')(loklakBot)
+redisClient.on('connect', () => {
+  console.log("connected to the redis database 👍")
+  // loklakBotModules
+  require('./lib/loklakTelegram.js')(loklakBot, redisClient)
+})
